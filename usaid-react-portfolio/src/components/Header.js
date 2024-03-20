79a72b9aeb0311b441765e6b7bd382e05c010952
UsaidAl <ali_usaid98@hotmail.com>
2024-03-20 22:49:58 +0000

    Header templates updated

---
+++ usaid-react-portfolio/src/components/Header.js
@@ -0,0 +1,19 @@
+// Header.js
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Your Name</h1>
+      <nav>
+        <ul>
+          <li><a href="#about">About</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          {/* Add more navigation links as needed */}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
