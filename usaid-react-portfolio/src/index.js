654cf47d569d3cf4edee35fe38a1f088dca4c040
UsaidAl <ali_usaid98@hotmail.com>
2024-03-21 02:49:52 +0000

    error handling

--- usaid-react-portfolio/src/index.js
+++ usaid-react-portfolio/src/index.js
@@ -1,14 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Resume from "./pages/Resume";
+import Contact from "./pages/Contact";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/",
+    element: <About />,
+  },
+  {
+    path: "/",
+    element: <Portfolio />,
+  },
+  {
+    path: "/",
+    element: <Resume/>,
+  },
+  {
+    path: "/",
+    element: <Contact/>,
+  },
+  
+
+  
+
+]);
+
+const root =
+ ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={routes} />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
